0962304977467865f7ed46a5f440189b354243a5
Anand Mohan <99183883+Anandmohan7@users.noreply.github.com>
2025-03-03 20:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
-  // your code here
+    // Trim leading spaces and find the first space index
+    s = s.trim();
+    let spaceIndex = s.indexOf(" ");
+
+    // If there is no space, return the entire string
+    return spaceIndex === -1 ? s : s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
